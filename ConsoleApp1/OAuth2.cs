3c6d877b2e37d627099b9e892d2f1b2369a94860
gicaCean <gicaCean@LAPTOP>
2022-02-20 16:14:58 +0200

    Add project files.

---
+++ ConsoleApp1/OAuth2.cs
@@ -0,0 +1,104 @@
+﻿using Nito.AsyncEx;
+using Newtonsoft.Json;
+using SpotifyCompanion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SpotifyCompanion
+{
+    public static class OAuth2
+    {
+        private static AccessTokenModel _accessTokenModel;
+        public static AccessTokenModel Authorize()
+        {
+            var Scope = new List<string>
+            {
+                Scopes.UserReadEmail, Scopes.UserReadPrivate, Scopes.PlaylistReadPrivate,
+                Scopes.PlaylistReadCollaborative, Scopes.UserReadCurrentlyPlaying, Scopes.UserReadPlaybackState
+            };
+
+
+            HttpListener Listener = new HttpListener();
+            Listener.Prefixes.Add(AppDetails.RedirectUri);
+            Listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+
+            Dictionary<string, string> RequestData = new Dictionary<string, string>
+            {
+                { "response_type", "code" },
+                { "client_id", AppDetails.ClientId },
+                { "scope", string.Join(" ", Scope) },
+                { "redirect_uri", AppDetails.RedirectUri }
+            };
+
+            var queryString = string.Join("&", RequestData.Select(KeyValPair => $"{KeyValPair.Key}={HttpUtility.UrlEncode(KeyValPair.Value)}"));
+            string Url = $"https://accounts.spotify.com/authorize?{queryString}";
+
+            System.Diagnostics.Process.Start(Url);
+
+            Listener.Start();
+            AsyncContext.Run(() => ListenLoop(Listener));
+            Listener.Stop();
+
+            return _accessTokenModel;
+        }
+
+        private static async Task ListenLoop(HttpListener Listener)
+        {
+            while (true)
+            {
+                var context = await Listener.GetContextAsync();
+                var query = context.Request.QueryString;
+
+                if (query != null && query.Count > 0)
+                {
+                    if (!string.IsNullOrEmpty(query["code"]))
+                    {
+
+                        _accessTokenModel = await GetToken(query["code"]);
+
+                        break;
+                    }
+                    else if (!string.IsNullOrEmpty(query["error"]))
+                    {
+                        var _errorResult = string.Format("{0}: {1}", query["error"], query["error_description"]);
+                        Console.WriteLine(_errorResult);
+                        throw new HttpRequestException(_errorResult);
+                    }
+                }
+            }
+        }
+
+        private static async Task<AccessTokenModel> GetToken(string code)
+        {
+            string url = "https://accounts.spotify.com/api/token";
+            Dictionary<string, string> RequestData = new Dictionary<string, string>
+            {
+                { "grant_type", "authorization_code" },
+                { "code", code },
+                { "redirect_uri", $"{AppDetails.RedirectUri}" }
+            };
+
+            FormUrlEncodedContent RequestBody = new FormUrlEncodedContent(RequestData);
+
+            using (HttpResponseMessage Response = await SpotifyClient.Client.PostAsync(url, RequestBody))
+            {
+                if (Response.IsSuccessStatusCode)
+                {
+                    AccessTokenModel AccessToken = await Response.Content.ReadAsAsync<AccessTokenModel>();
+
+                    return AccessToken;
+                }
+                else
+                {
+                    throw new HttpRequestException(Response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/SpotifyClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyCompanion
+{
+    public static class SpotifyClient
+    {
+        public static HttpClient Client { get; set; }
+        
+        public static void Initialize()
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri(AppDetails.BaseAdress);
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{AppDetails.ClientId}:{AppDetails.ClientSecret}"));
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+
+            var LoginInfo = OAuth2.Authorize();
+            Console.WriteLine(LoginInfo.access_token);
+        }
+    }
+}
